682042d44a9281614256a6c11656847627d2dabe
Danilo <danilot.89@gmail.com>
2019-05-04 16:38:35 +0200

    Bino SectionThree

--- src/app/pages/2-bino/2-components/3-section-three/section-three.component.ts
+++ src/app/pages/2-bino/2-components/3-section-three/section-three.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 
+import {
+  faFillDrip,
+  faFeatherAlt,
+  faCameraRetro
+} from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: "section-three",
   templateUrl: "./section-three.component.html",
-  styleUrls: ["./section-three.component.scss"]
+  styleUrls: ["./section-three.component.scss", "./../../1-page/fonts.scss"]
 })
 export class SectionThreeComponent implements OnInit {
   constructor() {}
+
+  faFillDrip    = faFillDrip;
+  faFeatherAlt   = faFeatherAlt;
+  faCameraRetro   = faCameraRetro;
 
   ngOnInit() {}
 }
