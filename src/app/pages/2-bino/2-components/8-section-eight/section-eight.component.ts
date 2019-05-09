29a0bcaba9816c0dc6d500223cc883968189930a
Danilo <danilot.89@gmail.com>
2019-05-09 20:49:21 +0200

    Bino SectionEight

--- src/app/pages/2-bino/2-components/8-section-eight/section-eight.component.ts
+++ src/app/pages/2-bino/2-components/8-section-eight/section-eight.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "section-eight",
   templateUrl: "./section-eight.component.html",
-  styleUrls: ["./section-eight.component.scss"]
+  styleUrls: ["./section-eight.component.scss" , "./../../1-page/fonts.scss"]
 })
 export class SectionEightComponent implements OnInit {
   constructor() {}
