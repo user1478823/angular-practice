b9ba27888c63ebcfc4c2b11ec47f6d9c9009bf7a
Danilo <danilot.89@gmail.com>
2019-05-10 14:23:53 +0200

    Sports SectionThree

--- src/app/pages/3-sports/2-components/3-section-three/section-three.component.ts
+++ src/app/pages/3-sports/2-components/3-section-three/section-three.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "section-three",
   templateUrl: "./section-three.component.html",
-  styleUrls: ["./section-three.component.scss"]
+  styleUrls: ["./section-three.component.scss", "./../../1-page/fonts.scss"]
 })
 export class SectionThreeComponent implements OnInit {
   constructor() {}
