c941f570796f746f5810cf4d1f4f9c1736de3c77
Danilo <danilot.89@gmail.com>
2019-05-18 19:45:43 +0200

    Agency SectionSix

--- src/app/pages/4-agency/2-components/8-footer/section-footer.component.ts
+++ src/app/pages/4-agency/2-components/8-footer/section-footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "agency-footer",
   templateUrl: "./section-footer.component.html",
-  styleUrls: ["./section-footer.component.scss"]
+  styleUrls: ["./section-footer.component.scss", "./../../1-page/fonts.scss"]
 })
 export class SectionFooterComponent implements OnInit {
   constructor() {}
