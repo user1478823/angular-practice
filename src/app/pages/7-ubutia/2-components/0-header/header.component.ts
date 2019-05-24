01b4d40118fe81c63820606e9f688fc2f366833b
Danilo <danilot.89@gmail.com>
2019-05-24 23:21:41 +0200

    Ubutia Header

--- src/app/pages/7-ubutia/2-components/0-header/header.component.ts
+++ src/app/pages/7-ubutia/2-components/0-header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "ubutia-header",
   templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"]
+  styleUrls: ["./header.component.scss", "./../../1-page/fonts.scss"]
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
