d03bdec268f7070a6f8115b02c5b2d97fbaef2ec
Danilo <danilot.89@gmail.com>
2019-05-30 12:38:48 +0200

    Interact SectionThree

--- src/app/pages/6-interact/2-components/3-section-three/section-three.component.ts
+++ src/app/pages/6-interact/2-components/3-section-three/section-three.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  faCode,
+  faTerminal,
+  faLeaf,
+  faBurn,
+  faBroom,
+  faComment
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "section-three",
   templateUrl: "./section-three.component.html",
-  styleUrls: ["./section-three.component.scss"]
+  styleUrls: ["./section-three.component.scss", "./../../1-page/fonts.scss"]
 })
 export class SectionThreeComponent implements OnInit {
   constructor() {}
+
+  faCode      = faCode;
+  faTerminal  = faTerminal;
+  faLeaf      = faLeaf;
+  faBroom     = faBroom;
+  faBurn      = faBurn;
+  faComment   = faComment;
 
   ngOnInit() {}
 }
