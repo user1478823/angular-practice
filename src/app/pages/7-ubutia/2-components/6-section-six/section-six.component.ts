95effef6df200a3118f2ca6a2bdb60f7799b3488
Danilo <danilot.89@gmail.com>
2019-06-01 12:13:03 +0200

    Ubutia SectionSix

--- src/app/pages/7-ubutia/2-components/6-section-six/section-six.component.ts
+++ src/app/pages/7-ubutia/2-components/6-section-six/section-six.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "section-six",
   templateUrl: "./section-six.component.html",
-  styleUrls: ["./section-six.component.scss"]
+  styleUrls: ["./section-six.component.scss", "./../../1-page/fonts.scss"]
 })
 export class SectionSixComponent implements OnInit {
   constructor() {}
