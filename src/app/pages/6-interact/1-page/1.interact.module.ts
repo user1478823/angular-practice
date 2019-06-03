e34d8328470f234b8db8bf44e95c4786a18a4bba
Danilo <danilot.89@gmail.com>
2019-06-03 13:08:42 +0200

    Interact Footer

--- src/app/pages/6-interact/1-page/1.interact.module.ts
+++ src/app/pages/6-interact/1-page/1.interact.module.ts
@@ -1,9 +1,9 @@
 import { NgModule }               from "@angular/core";
 import { CommonModule }           from "@angular/common";
 import { RouterModule }           from "@angular/router";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { FontAwesomeModule }      from "@fortawesome/angular-fontawesome";
 
-import { InteractComponent }          from "./2.interact.component";
+import { InteractComponent }      from "./2.interact.component";
 
 import { HeaderComponent }        from "../2-components/0-header/header.component";
 import { SectionOneComponent }    from "../2-components/1-section-one/section-one.component";
@@ -13,7 +13,7 @@ import { SectionFourComponent }   from "../2-components/4-section-four/section-f
 import { SectionFiveComponent }   from "../2-components/5-section-five/section-five.component";
 import { SectionSixComponent }    from "../2-components/6-section-six/section-six.component";
 import { SectionSevenComponent }  from "../2-components/7-section-seven/section-seven.component";
-import { SectionFooterComponent } from "../2-components/8-footer/footer.component";
+import { FooterComponent }        from "../2-components/8-footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -26,7 +26,7 @@ import { SectionFooterComponent } from "../2-components/8-footer/footer.componen
     SectionFiveComponent,
     SectionSixComponent,
     SectionSevenComponent,
-    SectionFooterComponent
+    FooterComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/6-interact/2-components/8-footer/footer.component.ts
+++ src/app/pages/6-interact/2-components/8-footer/footer.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+  faYoutube
+} from "@fortawesome/fontawesome-free-brands";
 
 @Component({
   selector: "interact-footer",
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.scss"]
 })
-export class SectionFooterComponent implements OnInit {
+export class FooterComponent implements OnInit {
   constructor() {}
+
+  faFacebookF  = faFacebookF;
+  faTwitter    = faTwitter;
+  faLinkedinIn = faLinkedinIn;
+  faYoutube    = faYoutube;
 
   ngOnInit() {}
 }
